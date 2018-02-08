690fad4d788c2a71b6e97a5deb70b795544d84c2
Alexandre Freire <xandrefreire@gmail.com>
2018-02-08 21:53:17 +0000

    Add Character model and tests

---
+++ Westeros/Domain/Character.swift
@@ -0,0 +1,11 @@
+//
+//  Character.swift
+//  Westeros
+//
+//  Created by Alexandre Freire on 08/02/2018.
+//  Copyright © 2018 Alexandre Freire. All rights reserved.
+//
+
+import Foundation
+
+typealias Character = String
